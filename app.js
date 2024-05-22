cdb5fed0f23835ac6923397e33f2badf0efebd69
dodoschnau <huangmeisin@gmail.com>
2024-05-22 13:39:01 +0800

    feat(handlebars): initialize handlebars layout

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
 const db = require('./models')
 const Todo = db.Todo
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './view')
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 })
 
 app.get('/todos', (req, res) => {
